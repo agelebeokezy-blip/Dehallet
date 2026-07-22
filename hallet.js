e5e15f93ba5761b47e3869173465c1140b481514
agelebeokezy-blip <agelebeokezy@gmail.com>
2026-07-22 19:00:15 +0100

    homepage modifications

--- hallet.js
+++ hallet.js
@@ -43,3 +43,37 @@ document.querySelectorAll('nav a').forEach(link => {
     link.classList.add('active');
   }
 });
+
+  
+// navbar scroll
+
+const header = document.querySelector('.navbar');
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > lastScrollY && currentScrollY > 100) {
+    // Scrolling DOWN
+    header.classList.add('show');
+  } else if (currentScrollY === 0) {
+    // Scrolling UP
+    header.classList.remove('show');
+  }
+
+  lastScrollY = currentScrollY;
+});
+
+// refresh from top
+
+// Disables the browser's automatic scroll restoration
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
+
+// Ensures the window scrolls to top on the load event
+window.scrollTo(0, 0);
+
+
+
--- script2.js
+++ script2.js
@@ -146,3 +146,60 @@ explore.addEventListener('click', () =>{
     }
   })
 
+  
+// navbar scroll
+
+const header = document.querySelector('.navbar');
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > lastScrollY && currentScrollY > 100) {
+    // Scrolling DOWN
+    header.classList.add('show');
+  } else if (currentScrollY === 0) {
+    // Scrolling UP
+    header.classList.remove('show');
+  }
+
+  lastScrollY = currentScrollY;
+});
+
+// refresh from top
+
+// Disables the browser's automatic scroll restoration
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
+
+// Ensures the window scrolls to top on the load event
+window.scrollTo(0, 0);
+
+// page scroll
+
+const hiddenElements = document.querySelectorAll('.scroll');
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach((entry) => {
+
+    if(entry.isIntersecting){
+
+      entry.target.classList.add('show');
+
+    }
+
+  });
+
+});
+
+hiddenElements.forEach((el, index) => {
+
+  el.style.transitionDelay = `${index * 0.1}s`;
+
+  observer.observe(el);
+
+});
+
